4fa5931dd203eeebd6d1d0bba341866bcdf2b657
amurromanov-tech <amur.romanov@gmail.com>
2026-08-20 14:41:32 +0900

    Update SettingsActivity.kt

--- app/src/main/java/com/family/base/ui/SettingsActivity.kt
+++ app/src/main/java/com/family/base/ui/SettingsActivity.kt
@@ -16,6 +16,7 @@ import com.family.base.data.remote.YandexDiskApi
 import com.family.base.databinding.ActivitySettingsBinding
 import com.family.base.ui.viewmodel.MainViewModel
 import com.family.base.util.Logger
+import com.family.base.BuildConfig
 import com.family.base.util.UpdateManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
