9307d4f0b544877908cfc290c285a5e62737aa5a
amurromanov-tech <amur.romanov@gmail.com>
2026-09-18 16:44:13 +0900

    Update build.gradle.kts
    изменил редактирование предмета

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.family.base"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7   // увеличивать при каждом обновлении
-        versionName = "2.1.0"   // версия для пользователя
+        versionCode = 8   // увеличивать при каждом обновлении
+        versionName = "2.2.0"   // версия для пользователя
 
         manifestPlaceholders["appAuthRedirectScheme"] = "com.family.base"
     }
