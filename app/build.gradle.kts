12b462d39927881d2c7f05b4e275d54d4fa9553e
amurromanov-tech <amur.romanov@gmail.com>
2026-09-21 12:30:36 +0900

    Update build.gradle.kts
    добавлены зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,9 @@ dependencies {
 
     // ML Kit для сканирования штрих-кодов
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
+   // ... остальные зависимости
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
 }
