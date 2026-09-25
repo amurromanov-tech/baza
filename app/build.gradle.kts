a1d7764d68eb571f6eca08945205447b7a82a5d9
amurromanov-tech <amur.romanov@gmail.com>
2026-09-25 10:50:52 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.family.base"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20   // увеличивать при каждом обновлении
-        versionName = "4.0.4"   // версия для пользователя
+        versionCode = 21   // увеличивать при каждом обновлении
+        versionName = "4.1.0"   // версия для пользователя
 
         manifestPlaceholders["appAuthRedirectScheme"] = "com.family.base"
     }
